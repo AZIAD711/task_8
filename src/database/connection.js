96879ed6489e8beed2bba5214c67cc2fb0788091
zeyad <aziad711@gmail.com>
2026-06-16 12:51:43 +0300

    ✅ ADD CONNCETION OF DATABASE

---
+++ src/database/connection.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose"
+
+export const connectionOfDatabase=async()=> {
+    // TODO : DATABASE CONNECTION OF URL 
+    const databaseUrl = "mongodb://localhost:27017/route_task_8"
+    // TODO : CHECK CONNECTION OF DATABASE
+    try{
+        await mongoose.connect(databaseUrl,{
+            serverSelectionTimeoutMS: 6000,
+            maxPoolSize:10
+        })
+        console.log("✅ Successfully connected to database")
+    }
+    catch(error){
+        console.error("❌ Error connecting to database:", error)
+    }
+}
